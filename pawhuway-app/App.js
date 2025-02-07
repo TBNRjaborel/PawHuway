65ee962bd25e0718a303e1a148719d26ac0f122e
Gabriel Magdugo <magdugogabriel@gmail.com>
2025-02-08 01:12:56 +0800

    added frontend of login page

--- pawhuway-app/App.js
+++ pawhuway-app/App.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Image, Button} from 'react-native';
+
 
 
 const LoginScreen = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
   return(
-    <View></View>
+    <SafeAreaView>
+      <View>
+        <View>
+          <Image source={require('./assets/pictures/paw-logo.png')} style = {styles.logo} alt="logo"/>
+        </View>
+
+        <View style = {styles.form}>
+          <View style = {styles.input}>
+            <Text style = {styles.inputLabel}>Email Address</Text>
+            <TextInput style ={styles.inputControl}
+              value = {form.email}
+              onChangeText = {email => setForm({ ...form,email})}
+              placeholder='magdugogabriel@gmail.com'
+              />
+          </View>
+
+          <View style = {styles.input}>
+            <Text style = {styles.inputLabel}>Password</Text>
+            <TextInput style ={styles.inputControl}
+              value = {form.email}
+              onChangeText = {password => setForm({ ...form,password})}
+              placeholder='*******'
+              />
+          </View>
+
+          <View>
+            <Button color = '#F9FE62' title='Log In' width = '80%'/ >
+
+
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
 
   );
 };
@@ -14,9 +52,8 @@ const LoginScreen = () => {
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>EYYY NIGANA!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <LoginScreen />
     </View>
   );
 }
@@ -24,8 +61,49 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFFAD6',
   },
+
+  logo: {
+    width: 350,
+    height: 350,
+    alignSelf: 'center',
+    // borderRadius: 200, 
+    marginTop: 100,
+    marginBottom: -50,
+  },
+  inputControl: {
+    backgroundColor: '#FFFFFF',
+    marginHorizontal: 20,
+    borderColor: '#808080',
+    borderWidth: 1/2,
+    paddingLeft: 15,
+    marginBottom: 20,
+    borderRadius: 10,
+    // alignSelf: 'center',
+    
+
+  },
+  inputLabel: {
+    marginBottom: 10,
+    textAlign: 'left',
+    paddingLeft: 25,
+    fontSize: 14,
+    fontWeight: '600',
+  },
+
+  form: {
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+    marginHorizontal: 50,
+    // paddingHorizontal: -90,
+    paddingVertical: 20,
+    borderColor: '#808080',
+    borderWidth: 1/2,
+  },
+
+  btn: {
+    color: '#F9FE62',
+  },
+  
 });
