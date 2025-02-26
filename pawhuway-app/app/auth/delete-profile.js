ba77565187a03e6202e17967d748046211bdba8a
Gabriel Magdugo <magdugogabriel@gmail.com>
2025-02-26 16:43:39 +0800

    intialized fiel for user delete

---
+++ pawhuway-app/app/auth/delete-profile.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, Image, Button, TouchableOpacity, Alert} from 'react-native';
+import { supabase }from '../../src/lib/supabase';
+import { Stack } from 'expo-router';
+import { useRouter } from 'expo-router';
+
+const deleteProfile = () => {
+    const router = useRouter();
+    return (
+
+    );
+}
