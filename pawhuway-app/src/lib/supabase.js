0391b062aaae32f2e64fc07bddb79d42c1475c3b
Mon <andyretuya13@gmail.com>
2025-06-03 00:11:49 +0800

    fixed expo dependencies

--- pawhuway-app/src/lib/supabase.js
+++ pawhuway-app/src/lib/supabase.js
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://vhpzcsquzwkruvewwmxf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZocHpjc3F1endrcnV2ZXd3bXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkxMDE0NzYsImV4cCI6MjA1NDY3NzQ3Nn0.9wFPrGfBCIXdW_XR6BDh2Y9UEaVj43X_JWldk8CTo9A';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
