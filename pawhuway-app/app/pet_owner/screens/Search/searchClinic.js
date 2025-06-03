2b14d80ab44526be56b53caef740b1e46354f500
Dongkor <165558096+Dongkor@users.noreply.github.com>
2025-06-04 06:06:16 +0800

    appointment

--- pawhuway-app/app/pet_owner/screens/Search/searchClinic.js
+++ pawhuway-app/app/pet_owner/screens/Search/searchClinic.js
@@ -1,28 +1,75 @@
-import { Text, View, ScrollView, StyleSheet, TouchableOpacity, Image, Pressable, Dimensions } from 'react-native';
-import React, { useState } from 'react';
+import { Text, View, ScrollView, StyleSheet, TouchableOpacity, Image, Dimensions, ActivityIndicator } from 'react-native';
+import React, { useState, useEffect } from 'react';
 import { SearchBar } from 'react-native-elements';
 import { Stack } from 'expo-router';
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { StatusBar } from 'expo-status-bar';
 import DateTimePicker from '@react-native-community/datetimepicker';
+import { useRouter } from "expo-router";
+import { supabase } from '../../../../src/lib/supabase';
 
 export default function SearchClinic() {
+    const router = useRouter()
     const [search, setSearch] = useState('');
     const today = new Date();
+    const [clinisList, setClinicsList] = useState([]);
+    const [loading, setLoading] = useState(true);
     const [checkInDate, setCheckInDate] = useState(today);
     const [checkOutDate, setCheckOutDate] = useState(today);
     const [showCheckIn, setShowCheckIn] = useState(false);
     const [showCheckOut, setShowCheckOut] = useState(false);
 
+
     const minDate = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
     const maxDate = new Date(today.getFullYear() + 2, today.getMonth(), today.getDate());
 
+    useEffect(() => {
+        const fetchClinics = async () => {
+            try {
+                const { data: { user }, error } = await supabase.auth.getUser();
+                if (error) {
+                    console.error("Error fetching user:", error.message);
+                    return;
+                }
+                // console.log("User email:", user.email);
+
+                // Fetch the clinics
+                const { data: clinicData, error: clinicError } = await supabase
+                    .from('vet_clinics')
+                    .select('*')
+                    .limit(5);
+
+                if (clinicError) {
+                    console.error("Error fetching clinics:", clinicError.message);
+                    return;
+                }
+
+                // console.log("Clinics data:", clinicData);
+                setClinicsList(clinicData);
+            } catch (error) {
+                console.error("Error fetching clinics:", error.message);
+            } finally {
+                setLoading(false);
+            }
+            // co
+        };
+
+        fetchClinics();
+        // console.log("hey", clinic)
+    }, []);
     return (
         <View style={styles.container}>
             <Stack.Screen options={{ headerShown: false }} />
             <View style={styles.searchContainer}>
                 <View style={styles.search}>
+                    <TouchableOpacity style={styles.backButton}
+                        onPress={() => { router.push("/pet_owner/dashboard-v2") }}
+                    >
+                        <AntDesign name="home" size={24} color="black" />
+                    </TouchableOpacity>
                     <View style={styles.location}>
-                        <Text style={{ fontFamily: 'Poppins Light' }}>Location</Text>
+                        <Text style={{ fontFamily: 'Poppins Light', fontSize: 20, color: '#3C3C4C', marginLeft: 12, marginBottom: 12 }}>Need a clinic?</Text>
+                        <Text style={{ fontFamily: 'Poppins Light', fontSize: 30, color: '#3C3C4C', marginLeft: 12 }}>Let's find you one for your pet.</Text>
                         <SearchBar
                             placeholder="Search activities..."
                             onChangeText={setSearch}
@@ -32,7 +79,8 @@ export default function SearchClinic() {
                                 backgroundColor: 'transparent',
                                 borderBottomColor: 'transparent',
                                 borderTopColor: 'transparent',
-                                width: '105%',
+                                height: 60,
+                                width: '100%',
                                 alignSelf: 'center'
                             }}
                             inputContainerStyle={{
@@ -47,54 +95,37 @@ export default function SearchClinic() {
                             }}
                         />
                     </View>
-
-                    <View style={styles.datePickers}>
-                        <View>
-                            <Text style={{ fontFamily: 'Poppins Light' }}>Check-in</Text>
-                            <Pressable onPress={() => setShowCheckIn(true)}>
-                                <Text style={{ fontFamily: 'Poppins Light', color: 'blue' }}>
-                                    {checkInDate.toDateString()}
-                                </Text>
-                            </Pressable>
-                            {showCheckIn && (
-                                <DateTimePicker
-                                    value={checkInDate}
-                                    mode="date"
-                                    display="default"
-                                    minimumDate={minDate}
-                                    maximumDate={maxDate}
-                                    onChange={(event, selectedDate) => {
-                                        setShowCheckIn(false);
-                                        if (selectedDate) setCheckInDate(selectedDate);
-                                    }}
-                                />
-                            )}
-                        </View>
-
-                        <View>
-                            <Text style={{ fontFamily: 'Poppins Light' }}>Check-out</Text>
-                            <Pressable onPress={() => setShowCheckOut(true)}>
-                                <Text style={{ fontFamily: 'Poppins Light', color: 'blue' }}>
-                                    {checkOutDate.toDateString()}
-                                </Text>
-                            </Pressable>
-                            {showCheckOut && (
-                                <DateTimePicker
-                                    value={checkOutDate}
-                                    mode="date"
-                                    display="default"
-                                    minimumDate={minDate}
-                                    maximumDate={maxDate}
-                                    onChange={(event, selectedDate) => {
-                                        setShowCheckOut(false);
-                                        if (selectedDate) setCheckOutDate(selectedDate);
-                                    }}
-                                />
-                            )}
-                        </View>
-                    </View>
                 </View>
             </View>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                {loading ? (
+                    <ActivityIndicator size="large" color="#3C3C4C" style={{ marginTop: 20 }} />
+                ) : (
+                    clinisList.map((clinic, index) => (
+                        <TouchableOpacity
+                            key={index}
+                            style={styles.card}
+                            onPress={() => router.push(`/pet_owner/screens/Search/clinic-details?clinicId=${clinic.id}`)}
+                        >
+                            <View style={{ flexDirection: "row", justifyContent: 'flex-start', marginHorizontal: 20, alignItems: 'center', height: '100%' }}>
+                                <Image
+                                    // source={{ uri: clinic.clinic_image || '../../assets/pictures/vet_clinic_1' }}
+                                    source={
+                                        clinic.clinic_image
+                                            ? { uri: clinic.clinic_image }
+                                            : require('../../../../assets/pictures/vet_clinic_1.png')
+                                    }
+                                    style={{ width: 100, height: 100, borderRadius: 20, marginRight: 20 }}
+                                />
+                                <View style={{ flex: 1, alignItems: 'flex-start', height: '100%', paddingVertical: 20 }}>
+                                    <Text style={{ fontFamily: 'Poppins', fontSize: 18, color: '#3C3C4C' }}>{clinic.clinic_name}</Text>
+                                    <Text style={{ fontFamily: 'Poppins Light', fontSize: 18, color: '#3C3C4C' }}>{clinic.clinic_address}</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    ))
+                )}
+            </ScrollView>
         </View>
     );
 }
@@ -103,20 +134,67 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    card: {
+        width: "90%",
+        height: 130,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 20,
+        // marginVertical: 10,
+        alignSelf: 'center',
+        // flexDirection: 'row',
+        // backgroundColor: '#FFFFFF',
+        // borderRadius: 20,
+        shadowColor: '#3C3C4C',
+        shadowRadius: 8,
+        elevation: 10,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // width: '90%',
+        // alignSelf: 'center',
+        // height: 130,
+    },
+    backButton: {
+        position: "absolute",
+        top: 35,
+        left: 20,
+        zIndex: 10,
+        backgroundColor: "#FFFFFF",
+        padding: 10,
+        borderRadius: 50,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3,
+    },
     searchContainer: {
-        backgroundColor: 'yellow',
-        margin: '6%',
-        borderRadius: 20
+        backgroundColor: '#B3EBF2',
+        // margin: '6%',
+        height: '35%',
+        borderBottomLeftRadius: 35,
+        borderBottomRightRadius: 35,
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingBottom: 80,
+        marginTop: '5%',
+        // marginBottom: 100,
+        gap: 20,
+
     },
     search: {
-        marginHorizontal: '5%',
-        marginTop: '2%',
-        marginBottom: '5%',
+        // flexDirection: 'column',
+        // marginBottom: '20%',
+        // backgroundColor: 'red',
+        height: '100%',
+        justifyContent: 'flex-end',
     },
     location: {
-        marginHorizontal: '3%',
-        marginTop: '3%',
-        marginBottom: '2%'
+        width: '90%',
+        alignSelf: 'center',
+        marginBottom: '5%',
     },
     datePickers: {
         flexDirection: 'row',
